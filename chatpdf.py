a7aed39608a45df5447287e0dd7da06078564f84
karim-rkha <karimrkha@gmail.com>
2023-06-12 22:07:15 +0200

    Update chatpdf.py

--- chatpdf.py
+++ chatpdf.py
@@ -24,7 +24,7 @@ import PyPDF2
 from langchain.document_loaders import PyPDFLoader
 
 sys.modules['pypdf'] = PyPDF2
-
+PyPDF2.PdfReader = PyPDF2.PdfFileReader
 
 api_key = st.secrets["general"]["API_KEY"]
 
